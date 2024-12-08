74648c5fca6ba9b3aa13e63aa229ebecbb64dc9d
Gagandeep <149200124+gagandeepkaur050@users.noreply.github.com>
2024-12-07 20:17:22 -0500

    Your commit message here

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.property_app_g02"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.property_app_g02"
