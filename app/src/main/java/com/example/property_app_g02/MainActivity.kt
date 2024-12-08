7c1f04ef40ca0382f7550a6ccec60313e3bb383c
Gagandeep <149200124+gagandeepkaur050@users.noreply.github.com>
2024-12-07 21:01:02 -0500

    Made changes to the lesson details screen

--- app/src/main/java/com/example/property_app_g02/MainActivity.kt
+++ app/src/main/java/com/example/property_app_g02/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.property_app_g02
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,5 +22,35 @@ class MainActivity : AppCompatActivity() {
         }
         supportActionBar!!.setTitle("Property")
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.settings_menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+
+            R.id.mi_incognito_tab -> {
+                Log.d("TESTING", "Incognito button clicked!")
+                return true
+            }
+            R.id.mi_history -> {
+                Log.d("TESTING", "History button clicked!")
+                return true
+            }
+            R.id.mi_clear_browsing_data -> {
+                Log.d("TESTING", "Browing Data cleared!")
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+
+
 
 }
